167ddccece716b366380e8c8d11e2f92f8cf7da0
Patricia Parker <pk.patricia@gmail.com>
2019-12-06 22:05:30 -0600

    Added more fields to Actors Table

--- src/main/java/com/example/sakilaexample/model/Actor.java
+++ src/main/java/com/example/sakilaexample/model/Actor.java
@@ -22,6 +22,12 @@ public class Actor {
     @Column(name="last_update")
     private Timestamp last_update;
 
+    @Column(name="Email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private int phone_number;
+
     // Empty Constructor
     public Actor() {
 
@@ -33,14 +39,14 @@ public class Actor {
         this.first_name = first_name;
         this.last_name = last_name;
         this.last_update = last_update;
+        this.email = email;
+        this.phone_number = phone_number;
     }
-
 
     // Getters and Setters
     public int getActor_id() {
         return actor_id;
     }
-
     public void setActor_id(int actor_id) {
         this.actor_id = actor_id;
     }
@@ -48,15 +54,11 @@ public class Actor {
     public String getFirst_name() {
         return first_name;
     }
-
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
-    public String getLast_name() {
-        return last_name;
-    }
-
+    public String getLast_name() { return last_name; }
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
@@ -64,15 +66,25 @@ public class Actor {
     public Timestamp getLast_update() {
         return last_update;
     }
-
     public void setLast_update(Timestamp last_update) {
         this.last_update = last_update;
+    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email;}
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
     }
 
     /* This is the method that is used to print out the object and its attributes */
     @Override
     public String toString() {
-        return "BlogPost [id=" + actor_id + ", first_name=" + first_name + ", last_name=" + last_name + ", last_update=" + last_update + "]";
+        return "BlogPost [id=" + actor_id + ", first_name=" + first_name + ", last_name=" + last_name + ", last_update=" + last_update + ", email=" +email +
+                "phone_number=" + phone_number +"]";
     }
 
 }
